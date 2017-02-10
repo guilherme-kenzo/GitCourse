3cffece99c352ea2a238dc867b497fd17d34b17e
Yukio Siraichi <sir.yukio@gmail.com>
2017-02-10 02:34:38 -0200

    ShowNames refactored.

--- ShowNames.py
+++ ShowNames.py
@@ -2,11 +2,15 @@
 
 import sys
 
-filename = sys.argv[1]
-f = open(filename, 'r')
+def Main():
+    filename = sys.argv[1]
+    f = open(filename, 'r')
+    
+    names = f.readlines()
+    for name in names:
+        name = name.replace('\n', '')
+        if name.strip() != "":
+            print(name)
 
-names = f.readlines()
-for name in names:
-    name = name.replace('\n', '')
-    if name.strip() != "":
-        print(name)
+if __name__ == '__main__':
+    Main()
